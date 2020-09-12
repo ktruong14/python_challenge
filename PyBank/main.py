457da729e85348638b96bf21c8a118eeb580fc34
ktruong14 <kev.truong14@gmail.com>
2020-09-12 14:34:59 +1000

    added main scripts

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,38 +3,47 @@ import os
 import csv
 
 budget_csv = os.path.join('PyBank','Resources','budget_data.csv')
-
-def analysis(budget_csv):
-    
-    month_name = str(budget_csv[0])
-    months = int(budget_csv[0])
-    profit_loss = int(budget_csv[1])
-
-    total_months = len(months)
-    net_total = sum(profit_loss)
-    avg_change = net_total / total_months
-    increase_profits = int(budget_csv[1].max())
-    decrease_profits = int(budget_csv[1].min())
-    
-    print("Financial Analysis")
-    print("---------------------------")
-    print("Total Months: " + total_months)
-    print("Total: $" + net_total)
-    print("Average Change: " + avg_change)
-    print("Greatest Increase in Profits: " + increase_profits)
-    print("Greatest Decrease in Profits: " + decrease_profits)
-
 # Reading CSV file
 with open(budget_csv, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader,None)
 
-    header = next(csvreader)
-
-    # Loop through data
+    # Declaring variables and calculations
+    date = []
+    profit_loss = []
     for row in csvreader:
-        if month_name == row[0]
+        profit_loss.append(int(row[1]))  
+        date.append(row[0])
 
-# Exporting to a txt file
+    increase_profits = max(profit_loss)
+    decrease_profits = min(profit_loss)
+    total_months = len(date)
+    net_total = sum(profit_loss)
+    avg_change = int(net_total) / int(total_months)
+    increase_profits_index = profit_loss.index(increase_profits) 
+    decrease_profits_index = profit_loss.index(decrease_profits)
+    increase_profits_date = date[increase_profits_index]
+    decrease_profits_date = date[decrease_profits_index]
+    
+    # Printing output
+    print("Financial Analysis")
+    print("---------------------------" )
+    print("Total Months: " + str(total_months))
+    print("Total: $" + str(net_total))
+    print("Average Change: " + str(avg_change))
+    print("Greatest Increase in Profits: " + str(increase_profits_date) + " " + "(" + str(increase_profits) + ")")
+    print("Greatest Decrease in Profits: " + str(decrease_profits_date) + " " + "(" + str(decrease_profits) + ")")
+
+# Exporting results to txt
+output = os.path.join("PyBank","analysis","output.txt")
+with open(output, 'w') as textfile:
+    textfile.write("Financial Analysis\n")
+    textfile.write("---------------------------\n")
+    textfile.write("Total Months: " + str(total_months))
+    textfile.write("\nTotal: $" + str(net_total))
+    textfile.write("\nAverage Change: " + str(avg_change))
+    textfile.write("\nGreatest Increase in Profits: " + str(increase_profits_date) + " " + "(" + str(increase_profits) + ")")
+    textfile.write("\nGreatest Decrease in Profits: " + str(decrease_profits_date) + " " + "(" + str(decrease_profits) + ")")
 
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -3,3 +3,87 @@ import os
 import csv
 
 election_csv = os.path.join('PyPoll','Resources','election_data.csv')
+
+voters = []
+candidates = []
+
+with open(election_csv,'r') as csvfile:
+    
+    csv_reader = csv.reader(csvfile,delimiter=',')
+    next(csv_reader,None)
+
+    # Declaring variables
+    
+    for row in csv_reader:
+        voters.append(int(row[0]))
+        candidates.append(str(row[2]))
+    
+no_of_voters = len(voters)
+khan_count = 0
+correy_count = 0
+li_count = 0
+tooley_count = 0
+
+    # For loop to count number of votes per candidates
+for row in candidates:
+        
+    if row == "Khan":        
+        khan_count = khan_count + 1
+    elif row == "Correy":
+        correy_count = correy_count + 1
+    elif row == "Li": 
+        li_count = li_count + 1
+    else:
+        tooley_count  = tooley_count + 1
+
+    if khan_count > correy_count:
+        winner = "Khan"
+    elif khan_count > li_count:
+        winner = "Khan"
+    elif khan_count > tooley_count:
+        winner = "Khan"
+    elif correy_count > li_count:
+        winner = "Correy"
+    elif correy_count > khan_count:
+        winner = "Correy"
+    elif correy_count > tooley_count:
+        winner = "Correy"
+    elif tooley_count > khan_count:
+        winner = "O'Tooley"
+    elif tooley_count > li_count:
+        winner = "O'Tooley" 
+    elif tooley_count > correy_count:
+        winner = "O'Tooley"
+    
+    khan_percent = (khan_count/no_of_voters) * 100
+    correy_percent = (correy_count/no_of_voters) * 100
+    li_percent = (li_count/no_of_voters) * 100
+    tooley_percent = (tooley_count/no_of_voters) * 100
+    
+    print("Election Results")
+    print("-----------------------")
+    print("Total Votes: " + str(no_of_voters))
+    print("-----------------------")
+    print("Khan: " + str(khan_percent) + "% " + "(" + str(khan_count) + ")")
+    print("Correy: " + str(correy_percent) + "% " + "(" + str(correy_count) + ")")
+    print("Li: " + str(li_percent) + "% " + "(" + str(li_count) + ")")
+    print("O'Tooley: " + str(tooley_percent) + "% " + "(" + str(tooley_count) + ")")
+    print("-----------------------")
+    print("Winner: " + winner)
+    print("-----------------------")
+
+# Exporting results to txt
+output = os.path.join("PyPoll","analysis","output.txt")
+
+with open(output, 'w') as textfile:
+    textfile.write("Election Results")
+    textfile.write("\n-----------------------")
+    textfile.write("\nTotal Votes: " + str(no_of_voters))
+    textfile.write("\n-----------------------")
+    textfile.write("\nKhan: " + str(khan_percent) + "% " + "(" + str(khan_count) + ")")
+    textfile.write("\nCorrey: " + str(correy_percent) + "% " + "(" + str(correy_count) + ")")
+    textfile.write("\nLi: " + str(li_percent) + "% " + "(" + str(li_count) + ")")
+    textfile.write("\nO'Tooley: " + str(tooley_percent) + "% " + "(" + str(tooley_count) + ")")
+    textfile.write("\n-----------------------")
+    textfile.write("\nWinner: " + winner)
+    textfile.write("\n-----------------------")
